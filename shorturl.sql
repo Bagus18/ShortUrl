e30c519688baf1b3bb929d284c59af0fe27fd1cc
Bagus <bagus@teknik.io>
2017-12-05 15:17:43 +0700

    fix table

--- shorturl.sql
+++ shorturl.sql
@@ -26,8 +26,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `shorturl_settings`
 --
 
-CREATE TABLE IF NOT EXISTS `shorturl_settings` (
-  `last_number` bigint(20) unsigned NOT NULL DEFAULT '0'
+CREATE TABLE IF NOT EXISTS `shorturl_settings` ( 
+  `last_number` bigint(20) unsigned NOT NULL DEFAULT '0',
+  KEY `last_number` (`last_number`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -35,58 +36,22 @@ CREATE TABLE IF NOT EXISTS `shorturl_settings` (
 --
 
 INSERT INTO `shorturl_settings` (`last_number`) VALUES
-(146),
-(146);
+(145),
+(145);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `shorturl_urls`
 --
-
 CREATE TABLE IF NOT EXISTS `shorturl_urls` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `url` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `code` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
   `alias` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
   `date_added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `st` int(11) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `shorturl_urls`
---
-
-INSERT INTO `shorturl_urls` (`id`, `url`, `code`, `alias`, `date_added`, `st`) VALUES
-(1, 'https://github.com/CollageTomato/ShortUrl', 'src', '', '2017-12-03 19:45:14', 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `shorturl_settings`
---
-ALTER TABLE `shorturl_settings`
-  ADD KEY `last_number` (`last_number`);
-
---
--- Indexes for table `shorturl_urls`
---
-ALTER TABLE `shorturl_urls`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `code` (`code`),
-  ADD KEY `alias` (`alias`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `shorturl_urls`
---
-ALTER TABLE `shorturl_urls`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  `st` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`),
+  KEY `alias` (`alias`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
